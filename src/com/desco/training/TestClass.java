656fc7d14473d304df36b6349984112319032e6c
Habibul Hassan Chowdhury <Habibul Hassan Chowdhury>
2017-04-23 18:57:02 +0600

    Initial commit

---
+++ src/com/desco/training/TestClass.java
@@ -0,0 +1,45 @@
+package com.desco.training;
+
+import java.util.Scanner;
+
+import javax.xml.bind.PrintConversionEvent;
+
+public class TestClass {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+
+	//	System.out.println("First Class");
+		Scanner input = new Scanner(system.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		int c = input.nextInt();
+	//	if (a>b) 
+	//	{
+		//	}
+	//	int a=5;
+	//	int b=129;
+	//	byte a=(byte)b;
+	//	String x ="10";
+	//	int a=Integer.parseInt(x);
+	//	System.out.println(a);
+	switch (1) {
+	case 1:
+		System.out.println("a");
+		break;
+	case 2:
+		System.out.println("b");
+		break;
+	case 3:
+		System.out.println("c");
+		break;
+	default:
+		break;
+	}	
+		
+		
+		
+		
+	}
+
+}
